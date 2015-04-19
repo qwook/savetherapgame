c15253adb5d993d386946a5f14e6c3eb163a8440
Will <bigredwill@gmail.com>
2015-04-19 00:18:15 -0700

    crude main menu

---
+++ SaveTheRapGame/Assets/LevelButton.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LevelButton : MonoBehaviour {
+
+	public bool locked = true;
+	public string sceneName;
+	private Button button;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void startLevel() {
+		if (GetComponent<Button> ().interactable) {
+			Application.LoadLevel(this.sceneName);
+		}
+	}
+
+	public void setLocked (bool toLock) {
+		GetComponent<Button> ().interactable = toLock;
+	}
+}
